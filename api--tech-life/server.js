9986ee29e66cd09ae2b67461c54688782a55c8a7
Raphael Rosa <raphael.rosa93@gmail.com>
2024-09-04 21:33:51 +0100

    estrutura API

---
+++ api--tech-life/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+/* db.on('error', console.log.bind(console, 'Connection Error'));
+db.once('open', () => {
+    console.log('db connection succesful!')
+}) */
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
