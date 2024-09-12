69deacd861ba9bd50800e3e3335fbe81ea10fd52
Raphael Rosa <raphael.rosa93@gmail.com>
2024-09-12 01:35:26 +0100

    allowEdition class

--- webapp--tech-life/src/app/classes-list/classes-list.component.ts
+++ webapp--tech-life/src/app/classes-list/classes-list.component.ts
@@ -1,16 +1,22 @@
-import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Component, Output, EventEmitter, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-classes-list',
   templateUrl: './classes-list.component.html',
   styleUrl: './classes-list.component.css'
 })
-export class ClassesListComponent {
+export class ClassesListComponent implements OnInit {
 
   @Output() selectClass = new EventEmitter();
   @Input() classList: Array<any> = [];
   @Output() removeClass = new EventEmitter();
   @Output() editClass = new EventEmitter();
+
+  allowEdition: boolean = false;
+
+  ngOnInit() {
+    this.allowEdition = JSON.parse(localStorage.getItem('user') || '{}')?.role !== 'Aluno';
+  }
 
   onEditClass(classData: any) {
     this.editClass.emit(classData);
